df7849bab84e91c908ddb37ec85402f4f9ad24a3
XavierLimaTI <xavierlima.ti@gmail.com>
2023-08-23 08:53:12 -0300

    Update scriptJs

--- Promise/script.js
+++ Promise/script.js
@@ -1,16 +1,16 @@
 // Fazer um Promise verificando se o usuário é o ADM utilizando: Nome = ADM e Senha = ADM
 
 const idCheck = new Promise((resolve, reject) =>{
-    var nome = ADM;
-    var senha = ADM;
+    var nome = "ADM";
+    var senha = "ADM";
 
-    if(nome === ADM){
+    if(nome === "ADM"){
         resolve("Usuario correto");
     }else{
         reject("Usuario incorreto");
     }
 
-    if(senha === ADM){
+    if(senha === "ADM"){
         resolve("Senha correta");
     }else{
         reject("Senha incorreta");
