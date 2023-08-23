8885ab6cfc6febe84a511e01aeb2b364eb810c2b
XavierLimaTI <xavierlima.ti@gmail.com>
2023-08-23 10:56:42 -0300

    update

---
+++ correção/ex2/js/index.js
@@ -0,0 +1,18 @@
+// Seletores
+const textoDigitado = document.getElementById("textoDigitado")
+const buscarPalavra = document.querySelector("#buscarPalavra")
+const substituirPalavra = document.getElementById("substituirPalavra")
+const trocarbtn = document.getElementById("trocar")
+const trocarTodosbtn = document.getElementById("trocarTodos")
+
+// eventos
+trocarbtn.addEventListener('click', trocarTexto)
+trocarTodosbtn.addEventListener('click', trocarTodosTexto)
+
+// functions
+function trocarTexto(){
+
+}
+function trocarTodosTexto(){
+
+}
