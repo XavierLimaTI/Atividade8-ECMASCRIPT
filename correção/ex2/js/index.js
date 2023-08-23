c7c51c398cb52f5e437388ddb3f933e5150260a0
XavierLimaTI <xavierlima.ti@gmail.com>
2023-08-23 11:21:46 -0300

    Update index.js

--- correção/ex2/js/index.js
+++ correção/ex2/js/index.js
@@ -6,13 +6,27 @@ const trocarbtn = document.getElementById("trocar")
 const trocarTodosbtn = document.getElementById("trocarTodos")
 
 // eventos
+// eventListener(interação, função)
 trocarbtn.addEventListener('click', trocarTexto)
 trocarTodosbtn.addEventListener('click', trocarTodosTexto)
 
 // functions
 function trocarTexto(){
+    let textoDigitadoValor = textoDigitado.value;
+    let buscarPalavraValor = buscarPalavra.value;
+    let substituirPalavraValor = substituirPalavra.value;
 
+    var textoFormatado = textoDigitadoValor.replace(buscarPalavraValor, substituirPalavraValor);
+    textoDigitado.value = textoFormatado;
+    
 }
+
 function trocarTodosTexto(){
+    let textoDigitadoValor = textoDigitado.value;
+    let buscarPalavraValor = buscarPalavra.value;
+    let substituirPalavraValor = substituirPalavra.value;
+
+    var textoFormatado = textoDigitadoValor.replaceAll(buscarPalavraValor, substituirPalavraValor);
+    textoDigitado.value = textoFormatado;
 
 }
